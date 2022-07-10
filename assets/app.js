ffa58f884bc385ada772b3bee66bdd8635bb3d97
Habibun Noby <habibun.noby@gmail.com>
2022-07-10 15:45:39 +0600

    feat: add encore

---
+++ assets/app.js
@@ -0,0 +1,24 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+// start the Stimulus application
+import './bootstrap';
+
+import React ,{Component}from 'react';
+import ReactDom from 'react-dom';
+
+class App extends Component {
+    render() {
+        return <h2>Lift History! <span> test </span></h2>;
+    }
+}
+console.log(App);
+
+ReactDom.render(<App/>, document.getElementById('root'));
