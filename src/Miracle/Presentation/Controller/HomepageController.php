b993aa96dc5752403e35af2e6266b5a274828ee3
Habibun Noby <habibun.noby@gmail.com>
2022-07-15 19:48:05 +0600

    feat: add translation

--- src/Miracle/Presentation/Controller/HomepageController.php
+++ src/Miracle/Presentation/Controller/HomepageController.php
@@ -7,6 +7,11 @@ use Symfony\Component\HttpFoundation\Response;
 
 class HomepageController extends AbstractController
 {
+    public function indexNoLocale(): Response
+    {
+        return $this->redirectToRoute('homepage', ['_locale' => 'en']);
+    }
+
     public function index(): Response
     {
         return $this->render('@miracle/homepage/index.html.twig');
