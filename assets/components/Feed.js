d3dd2ae3fda476ce621becd640c9b3f538d25861
Habibun Noby <habibun.noby@gmail.com>
2022-08-23 19:52:29 +0600

    feat: add random ayah

--- assets/components/Feed.js
+++ assets/components/Feed.js
@@ -1,9 +1,27 @@
 import React ,{Component}from 'react';
 import ReactDom from 'react-dom';
+import axios from 'axios';
 
 class Feed extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { verse: [], loading: true};
+    }
+
+    componentDidMount() {
+        this.getVerse();
+    }
+
+    getVerse() {
+        axios.get(`https://dev.iqra.com/en/api/quran/api/chapter/verse/random`).then(verse => {
+            console.log(verse);
+            this.setState({ verse: verse.data, loading: false})
+        })
+    }
+
     render() {
-        return <h2>Feed! <span> test </span></h2>;
+        console.log(this.state);
+        return <h2>Feed! <span> {this.state.verse.text} </span></h2>;
     }
 }
 
