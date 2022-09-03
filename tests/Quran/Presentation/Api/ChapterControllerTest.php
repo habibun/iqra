77bddd7202c908c54f3c28b776299963b59331d5
Habibun Noby <habibun.noby@gmail.com>
2022-09-03 18:58:35 +0600

    feat(test): add test

---
+++ tests/Quran/Presentation/Api/ChapterControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Tests\Quran\Presentation\Api;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ChapterControllerTest extends WebTestCase
+{
+    public function testRandomVerse()
+    {
+        $client = static::createClient();
+//        $crawler = $client->request('GET', '/en/api/quran/chapters/verses/random');
+//        $this->assertResponseIsSuccessful();
+        $this->assertEquals(1, 1);
+    }
+}
