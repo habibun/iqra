c42c3ad150fd48a45d215745da1b197467517c9e
Habibun Noby <habibun.noby@gmail.com>
2022-09-30 12:15:53 +0600

    fix(context): add group

--- src/Context/Domain/Repository/GroupRepositoryInterface.php
+++ src/Context/Domain/Repository/GroupRepositoryInterface.php
@@ -10,4 +10,8 @@ interface GroupRepositoryInterface
     public function add(Group $group);
 
     public function nextIdentity(): Uuid;
+
+    public function getTranslationByIsoCode(string $isoCode);
+
+    public function getByIdAndLanguageIso(string $id, string $languageIso);
 }
